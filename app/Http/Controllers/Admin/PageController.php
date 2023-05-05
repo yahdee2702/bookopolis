413bd7306cedc68cf945cc074ae83aa86b0c98df
yahdee2702 <yahdee2702@github.com>
2023-05-05 14:15:48 +0700

    Fixed some incovenience

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -9,7 +9,7 @@ use App\Models\Order;
 class PageController extends Controller
 {
     public function index() {
-        $activities = Activity::query()->with(['admin'])->get();
+        $activities = Activity::query()->with(['admin'])->orderBy('created_at', 'desc')->limit(10)->get();
         $orders = Order::query()->with(['user', 'book'])->get();
 
         return view("pages.admin.index", compact(['activities', 'orders']));
--- resources/views/components/book-card.blade.php
+++ resources/views/components/book-card.blade.php
@@ -3,7 +3,7 @@
     'bg-white' => !$selected,
     'bg-black text-white' => $selected,
 ]) wire:click="{{ !$selected ? $func . '(' . $data->id . ')' : $func . '(-1)' }}">
-    <img src="{{ $data->image }}" class="aspect-1 w-36 h-36 rounded-l-xl objcet-cover" alt="">
+    <img src="{{ $data->image }}" class="aspect-1 w-36 h-36 rounded-l-xl object-cover object-center" alt="">
     <div class="w-full h-full flex flex-col justify-between flex-1 px-4 py-3">
         <div>
             <h5 class="text-xl font-semibold mb-1">{{ $data->title }}</h5>
@@ -17,7 +17,7 @@
             'text-gray' => $selected,
         ])>
             <div class="flex flex-row gap-1 items-center">
-                <img src="{{ $data->seller->image }}" class="aspect-1 w-5 h-5 rounded-full object-cover" alt="">
+                <img src="{{ $data->seller->image }}" class="aspect-1 w-5 h-5 rounded-full object-cover object-center" alt="">
                 <span>{{ $data->seller->name }}</span>
             </div>
 
