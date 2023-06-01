42b60ad196f6f6612b6488c52f924a5b2b2f9f6b
yahdee2702 <yahdee2702@github.com>
2023-06-01 11:11:57 +0700

    Another fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(Request $request) {
-        $newRelease = Book::query()->orderBy('desc')->limit(3)->get();
+        $newRelease = Book::query()->orderBy('updated_at', 'desc')->limit(3)->get();
         $bestSeller = Book::query()->withCount('orders')->orderBy('orders_count', 'desc')->limit(3)->get();
 
         return view('pages.index', compact(['newRelease', 'bestSeller']));
